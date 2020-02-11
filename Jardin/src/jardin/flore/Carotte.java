e74d4c4111c7b8bc160b9a272c40fa15aba031cf
Tom Krepa <tom.krepa@ynov.com>
2020-02-11 20:02:14 +0100

    Jardin

---
+++ Jardin/src/jardin/flore/Carotte.java
@@ -0,0 +1,18 @@
+package jardin.flore;
+
+import java.util.HashMap;
+
+public class Carotte extends Vegetal implements IRacePure {
+
+	public Carotte() {
+		super();
+		dessin[3] = 'c';
+		dessin[4] = 'C';
+	}
+
+	@Override
+	public void seReproduire(HashMap<String, Integer> panier) {
+		panier.put("Carotte", panier.get("Carotte") + 3);
+	}
+
+}
--- Jardin/src/jardin/flore/Tomate.java
+++ Jardin/src/jardin/flore/Tomate.java
@@ -1,9 +1,9 @@
-package Flore;
+package jardin.flore;
 
 public class Tomate extends Vegetal {
-	
+
 	public Tomate() {
-		
+		super();
 		dessin[3] = 't';
 		dessin[4] = 'T';
 	}
